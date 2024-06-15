1a4772c35b43b538a1a212a509e4decd0e00698f
Slava Simonenko <slava.simonenko.01@gmail.com>
2024-06-15 18:05:20 +0900

    mini-app#2 create prisma schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { SongModule } from './song/song.module'
+import { PlaylistModule } from './playlist/playlist.module'
+import { ConfigModule } from '@nestjs/config'
+import { AuthModule } from './auth/auth.module'
+import { UsersModule } from './users/users.module'
+import { GoogleModule } from './google/google.module'
+import { MulterModule } from '@nestjs/platform-express'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		AuthModule,
+		SongModule,
+		PlaylistModule,
+		UsersModule,
+		GoogleModule,
+		ConfigModule.forRoot(),
+		MulterModule.register({
+			dest: './uploads' // Temporary folder for storing uploaded files
+		})
+	],
+	controllers: [AppController],
+	providers: [AppService]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import * as cookieParser from 'cookie-parser'
+import { ValidationPipe } from '@nestjs/common'
+import { json } from 'express'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 
 	app.setGlobalPrefix('api')
-	app.enableCors()
+
+	app.use(cookieParser())
+	app.use(json({ limit: '5mb' }))
+	app.useGlobalPipes(new ValidationPipe())
+
 	await app.listen(4000)
 }
 bootstrap()
